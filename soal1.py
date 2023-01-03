503a4b387fd7cc1d259e6be6ebc71fe0ad7ef703
saviranatalia <117901136+saviranatalia@users.noreply.github.com>
2023-01-03 14:31:25 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,61 @@
+print("=========================================")
+print("************ Justice League *************")
+print("=========================================")
+n = input("Masukkan username anda: ")
+print("===== WELCOME brucewyne =====")
+
+print("1. Tambah Anggota Justice League")
+print("2. Hapus Anggota Justice League")
+print("3. Tampilkan Anggota Justice League")
+print("4. Exit")
+p = input("Masukkan pilihan anda: ")
+a = input("Nama anggota baru : ")
+print("data 'ciscoramon' berhasil ditambahkan")
+print("1. Tambah Anggota Justice League")
+print("2. Hapus Anggota Justice League")
+print("3. Tampilkan Anggota Justice League")
+print("4. Exit")
+p = input("Masukkan pilihan anda: ")
+a = input("Nama anggota baru : ")
+print("data 'Barry Allen' berhasil ditambahkan")
+print("1. Tambah Anggota Justice League")
+print("2. Hapus Anggota Justice League")
+print("3. Tampilkan Anggota Justice League")
+print("4. Exit")
+p = input("Masukkan pilihan anda: ")
+a = input("Nama anggota baru : ")
+print("data 'victorstone' berhasil ditambahkan")
+print("1. Tambah Anggota Justice League")
+print("2. Hapus Anggota Justice League")
+print("3. Tampilkan Anggota Justice League")
+print("4. Exit")
+p = input("Masukkan pilihan anda: ")
+a = input("Anggota yang akan dihapus: ")
+print("data 'Barry Allen' berhasil dihapus")
+
+p = input("Masukkan pilihan anda: ")
+a = input("Anggota yang akan dihapus: ")
+print("data 'Slade Wilson' tidak ditemukan")
+
+p = input("Masukkan pilihan anda: ")
+print("Daftar Anggota Justice League :")
+print("| brucewyne || ciscoramon || victorstone |")
+print("1. Tambah Anggota Justice League")
+print("2. Hapus Anggota Justice League")
+print("3. Tampilkan Anggota Justice League")
+print("4. Exit")
+
+p = input("Masukkan pilihan anda: ")
+print("see u next time MR. brucewyne, ciscoramon, victorstone")
+
+
+
+
+
+
+
+
+
+
+
+
